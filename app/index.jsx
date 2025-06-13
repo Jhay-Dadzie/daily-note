b903d0958e22a5f8f336585fbec81d4e75d3d49f
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-13 04:07:59 +0000

    Updates when note is added

--- app/index.jsx
+++ app/index.jsx
@@ -1,19 +1,39 @@
 import { StyleSheet, Text, View, SafeAreaView, Image, TextInput, FlatList, Pressable } from "react-native";
-import Notes from "../components/note.json"
+import { Notes } from "@/components/note"
 import { Link, router } from "expo-router"
 import FontAwesome from "@expo/vector-icons/FontAwesome"
+import { useState, useEffect } from "react";
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export default function Index() {
+  const [notes, setNotes] = useState([]);
+  useEffect(() => {
+    const loadNotes = async () => {
+      try {
+        const savedNotes = await AsyncStorage.getItem('notes');
+        if (savedNotes) {
+          setNotes(JSON.parse(savedNotes).sort((a, b) => b.id - a.id));
+        } else {
+          setNotes(Notes.sort((a, b) => b.id - a.id) || []);
+        }
+      } catch (error) {
+        console.error('Failed to load notes', error);
+      }
+    };
+
+    loadNotes();
+  }, [[router.params?.refresh]]);
   return (
     <SafeAreaView style={styles.safeArea}>
       <FlatList 
-        data={Notes}
-        contentContainerStyle={Notes.length === 0 ? styles.emptyListContainer : null}
+        data={notes}
+        contentContainerStyle={notes.length === 0 ? styles.emptyListContainer : null}
         renderItem={({item}) => (
           <View style={styles.noteView}>
             <Text style={styles.title}>{item.title}</Text>
             <Text style={styles.body}>{item.body}</Text>
           </View>
+          
         )}
         ItemSeparatorComponent={() => <View style={{height: 5}} />}
         ListEmptyComponent={() => (
@@ -35,6 +55,17 @@ export default function Index() {
           </View>
         )}  
       />
+
+      {
+        notes.length > 0 && (
+        <Pressable style={styles.addNoteButton} onPress={() => router.push('/createNote')}>
+          <View style={{marginHorizontal: 'auto', height: 50, justifyContent: 'center', alignItems: 'center'}}>
+              <FontAwesome name='plus' size={25} color={'white'}/>
+              <Text style={{color: 'white', fontWeight: 'bold', fontSize: 12}}>Create Note</Text>
+          </View>
+        </Pressable>
+        )
+      }
     </SafeAreaView>
   );
 }
@@ -102,5 +133,18 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 16,
     fontWeight: 'bold'
+  },
+  addNoteButton: {
+    backgroundColor: '#ffa400',
+    width: 70,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '100%',
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 70,
+    right: 40
   }
 });
