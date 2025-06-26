744502e746a5ed2c35392c541c219df0389cbab5
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-26 03:18:14 +0000

    Added todo page

---
+++ app/(tabs)/toDo.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const toDo = () => {
+  return (
+    <View>
+      <Text>This is the to do page</Text>
+    </View>
+  )
+}
+
+export default toDo
