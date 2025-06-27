aba398424b77c0652721015f106b7388cc8bb3d0
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-27 21:56:03 +0000

    Added todo stack screen

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -2,17 +2,21 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerTitleStyle: {
-          fontWeight: 'bold'
-        }
-    }}>
+    <Stack>
       <Stack.Screen name="(tabs)" options={{
         headerShown: false,
       }}/>
       <Stack.Screen name="createNote" options={{
         presentation: 'modal',
         title: "Create Note",
+      }}/>
+      <Stack.Screen name="createReminder" options={{
+        presentation: 'modal',
+        title: "Create Reminder",
+      }}/>
+      <Stack.Screen name="createTodo" options={{
+        presentation: 'modal',
+        title: "Create Todo",
       }}/>
       
       <Stack.Screen name="dynamics/[notesId]" options={{
