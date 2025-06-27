8b56b7ba7f5f281e5eb96d1e46d0b66cf8ad83a1
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-27 15:33:21 +0000

    Added pages to create reminders and to-dos

---
+++ app/createReminder.jsx
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import styles from '@/components/styles/styles'
+
+const createReminder = () => {
+  return (
+    <View>
+      <Text>You can create your reminder here</Text>
+    </View>
+  )
+}
+
+export default createReminder
---
+++ app/createToDo.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import styles from '@/components/styles/styles'
+
+export default function createToDo() {
+  return (
+    <View>
+      <Text>You can create to do here</Text>
+    </View>
+  )
+}
