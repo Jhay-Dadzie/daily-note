48821b1e3a2be97c215c77938645f3f4d37c2789
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-27 15:23:47 +0000

    Grouped all styles for reusability

---
+++ components/styles/createPageStyles.jsx
@@ -0,0 +1,43 @@
+import { StyleSheet } from "react-native";
+
+const createPageStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    inputFieldContainer: {
+        flex: 1,
+        flexDirection: 'column',
+        position: 'relative'
+    },
+    inputField: {
+        padding: 15,
+        pointerEvents: 'auto'
+    },
+    titleInput: {
+        fontSize: 22,
+        fontWeight: 'bold',
+        color: '#656768'
+    },
+    bodyInput: {
+        fontSize: 16,
+        color: '#717272',
+        flex: 1,
+        textAlignVertical: 'top'
+    },
+    updateButton: {
+        backgroundColor: '#ffa400',
+        paddingVertical: 15,
+        paddingHorizontal: 15,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: '100%',
+        position: 'absolute',
+        marginBottom: 40,
+        bottom: 70,
+        right: 40
+
+    }
+
+})
+
+export default createPageStyles
---
+++ components/styles/styles.jsx
@@ -0,0 +1,85 @@
+import {StyleSheet, Platform } from 'react-native'
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 10
+  },
+  emptyListContainer: {
+    flex: 1,
+  },
+  emptyContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+    marginTop: -50,
+  },
+  emptyImage: {
+    width: 80,
+    height: 80,
+    marginBottom: 20,
+  },
+  emptyTextContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  emptyTitle: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  emptySubtitle: {
+    fontSize: 16,
+    color: '#717272',
+    marginTop: 5,
+  },
+  noteView: {
+    padding: 10,
+    borderWidth: 2,
+    borderRadius: 10,
+    borderColor: '#ffa400',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between'
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    color: '#656768'
+  },
+  body: {
+    fontSize: 16,
+    color: '#717272'
+  },
+  createNoteButton: {
+    flexDirection: 'row',
+    backgroundColor: '#ffa400',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 25,
+    alignItems: 'center',
+    gap: 10
+  },
+  createButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold'
+  },
+  addNoteButton: {
+    backgroundColor: '#ffa400',
+    width: 70,
+    paddingVertical: 10,
+    paddingHorizontal: 5,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '100%',
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: Platform.OS === 'ios' ? 90 : 70,
+    right: 40
+  }
+});
+
+export default styles
