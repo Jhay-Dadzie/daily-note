2a55c0051e30117ae3fb1e3567dddc0e519c35d6
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-06-30 19:09:03 +0000

    Added reminder index

---
+++ app/dynamics/reminderRoute/[remindersId].jsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function RemindersScreen() {
+  return (
+    <View>
+      <Text>[remindersId]</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
