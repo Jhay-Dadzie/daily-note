412decddabea22dc9dcace1cfd15873312630e95
Jhay-Dadzie <josephdadzie0002@gmail.com>
2025-07-28 21:42:04 +0000

    Declared constants of colors for simpliciy

---
+++ components/constants/themeColor.jsx
@@ -0,0 +1,18 @@
+export const themeColor = {
+    lightMode: {
+        background: 'white',
+        emptyTitle: 'black',
+        title: '#656768',
+        body: '#717272'
+    },
+    darkMode: {
+        background: '#151515',
+        emptyTitle: 'black',
+        title: '#f2f2f2',
+        body: '#ffffff'
+    },
+
+    colorTheme: {
+        color: '#ffa400'
+    }
+}
